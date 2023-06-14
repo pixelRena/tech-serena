2b4e2499e50dc33bf82f877221e0710131902a1c
Serena <98000871+pixelRena@users.noreply.github.com>
2023-06-13 21:13:04 -0500

    Update projects.utils.jsx

--- src/utils/projects.utils.jsx
+++ src/utils/projects.utils.jsx
@@ -46,7 +46,7 @@ const projects = {
                 "cover_alt": "https://res.cloudinary.com/dyqefy6t8/image/upload/v1665823921/ubsizpsqpqkyymxa23r6.jpg",
                 "cover": "../images/expertece.jpg"
             },
-            "websiteurl": "http://www.expertece-prototype.store",
+            "websiteurl": "http://expertece-prototype.store",
             "githuburl": "",
             "description": "Responsive web application designated to allow users to browse and purchase customized products. Implemented admin dashboard to manage products uploaded, customer database and payments"
         },
