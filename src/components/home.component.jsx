c230af86fae8a74adebc63ac12b5471453887eda
Serena <sdeannv@yahoo.com>
2026-02-24 02:51:27 -0800

    feat: update year, email, add .env variables for email

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -1,76 +1,91 @@
-import projects from "../utils/projects.utils";
-import { Fragment, useRef } from "react";
+import projects from "../utils/projects.utils"
+import { Fragment, useRef } from "react"
 
 const Home = () => {
-  const emailBtnRef = useRef(null);
+  const emailBtnRef = useRef(null)
 
   const handleEmailCopy = () => {
-    navigator.clipboard.writeText('dserena01@yahoo.com')
+    navigator.clipboard
+      .writeText(process.env.REACT_APP_EMAIL)
       .then(() => {
-        emailBtnRef.current.innerText = "Copied Successfully!";
+        emailBtnRef.current.innerText = "Copied Successfully!"
         setTimeout(() => {
-          emailBtnRef.current.innerText = "dserena01@yahoo.com";
+          emailBtnRef.current.innerText = process.env.REACT_APP_EMAIL
         }, 2000)
       })
       .catch((err) => {
-        emailBtnRef.current.innerText = "Failed to copy email :(";
+        emailBtnRef.current.innerText = "Failed to copy email :("
         setTimeout(() => {
-          emailBtnRef.current.innerText = "dserena01@yahoo.com";
+          emailBtnRef.current.innerText = process.env.REACT_APP_EMAIL
         }, 2000)
-      });
+      })
   }
 
-  return(
-  <div id="about-container">
-    <div id="about">
-      <div className="name"><span>.</span>SERENA</div>
-      <div className="bio">
-        <div className="title"><span>Web</span> Developer</div>
-        <div className="objective">
-          I help businesses get more reach by creating designs and bringing them to life while you work on what's most important.
+  return (
+    <div id="about-container">
+      <div id="about">
+        <div className="name">
+          <span>.</span>SERENA
+        </div>
+        <div className="bio">
+          <div className="title">
+            <span>Software</span> Engineer
+          </div>
+          <div className="objective">
+            I help businesses get more reach by creating designs and bringing
+            them to life while you work on what's most important.
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="projects-list"> 
+      <div className="projects-list">
         <h3 id="select-list-text">Projects:</h3>
-        {
-          projects["html"].map(project => {
-            return(
-              <Fragment key={project.title}>
-                <a href={project.websiteurl} 
+        {projects["html"].map((project) => {
+          return (
+            <Fragment key={project.title}>
+              <a
+                href={project.websiteurl}
                 target="_blank"
                 rel="noreferrer"
                 className="projects-list-title"
-                
-                ><span className="projects-list-type">{project.type} {"//"} </span>{project.title}
-                  <span className="projects-list-bullet"></span>
-                </a>
-              </Fragment>
-            )})
-        }
-        {
-            projects["react"].map(project => {
-            return(
-              <Fragment key={project.title}>
-                <a href={project.websiteurl} 
+              >
+                <span className="projects-list-type">
+                  {project.type} {"//"}{" "}
+                </span>
+                {project.title}
+                <span className="projects-list-bullet"></span>
+              </a>
+            </Fragment>
+          )
+        })}
+        {projects["react"].map((project) => {
+          return (
+            <Fragment key={project.title}>
+              <a
+                href={project.websiteurl}
                 target="_blank"
                 rel="noreferrer"
                 className="projects-list-title"
-                
-                ><span className="projects-list-type">{project.type} {"//"} </span> {project.title}
-                  <span className="projects-list-bullet"></span>
-                </a>
-              </Fragment>
-            )})
-        }
+              >
+                <span className="projects-list-type">
+                  {project.type} {"//"}{" "}
+                </span>{" "}
+                {project.title}
+                <span className="projects-list-bullet"></span>
+              </a>
+            </Fragment>
+          )
+        })}
+      </div>
+      <button
+        ref={emailBtnRef}
+        id="email-btn"
+        onClick={() => handleEmailCopy()}
+      >
+        {process.env.REACT_APP_EMAIL}
+      </button>
     </div>
-    <button 
-    ref={emailBtnRef}
-    id="email-btn" 
-    onClick={() => handleEmailCopy()}>dserena01@yahoo.com</button>
-  </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
